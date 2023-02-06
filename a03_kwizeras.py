a24cd08975d311e393b128d68d3d4b61e2099129
kwizeras <kwizeras@berea.edu>
2023-02-06 14:14:39 -0500

    soccer_field

--- a03_kwizeras.py
+++ a03_kwizeras.py
@@ -1,46 +1,74 @@
 import turtle
 
-def mountain():
+def field():
     t = turtle.Turtle()
-    t.speed(4)
+    t.speed(10)
     t.penup()
-    t.goto(-90, -300)
+    t.goto(-200, -200)
     t.pendown()
-    for i in range(4):
-        t.forward(400)
+    t.fillcolor("green")
+    t.begin_fill()
+    for i in range(2):
+        t.forward(300)
         t.left(90)
-        t.fd(10)
-def trail():
+        t.fd(500)
+        t.left(90)
+    t.end_fill()
+r=20
+def lines():
     t1 = turtle.Turtle()
-    t1.speed(1)
+    t1.speed(10)
     t1.penup()
-    t1.goto(-300, 180)
+    t1.goto(-200, 70)
     t1.pendown()
-    t1.pensize(30)
-    t1.pencolor("#D2BC91")
-    for i in range(5):
-        t1.right(40)
-        t1.forward(60)
-        t1.left(30)
-        t1.forward(60)
-def trees():
+    t1. fillcolor("#FEFDFC")
+    t1.pencolor("#FEFDFC")
+    t1.fd(300)
+    t1.bk(175)
+    t1.right(90)
+    t1.fd(2)
+    t1.circle(r)
+    t1.penup()
+    t1.left(90)
+    t1.fd(18)
+    t1.pendown()
+    t1.circle(radius=r/10)
+    t1.penup()
+    t1.goto(-100, 600)
+def golies():
     t2 = turtle.Turtle()
-    t2.speed(10)
     t2.penup()
-    t2.goto(-300, 250)
+    t2.goto(-120, 300)
     t2.pendown()
-    for i in range(3):
-        t2.left(80)
-        t2.fd(60)
-        t2.right(120)
-        t2.fd(60)
-        t2.right(120)
-        #t2.fd(63)
-        #t2.bk(31)
-       # t2.left(70)
-       # t2.fd(50)
-        #t2.left(30)
-        #t2.fd(40)
+    t2.pencolor("#FEFDFC")
+    for i in range(2):
+        t2.fd(140)
+        t2.right(90)
+        t2.fd(70)
+        t2.right(90)
+    t2.right(90)
+    t2.penup()
+    t2.fd(430)
+    t2.left(90)
+    t2.pendown()
+    for i in range(2):
+        t2.fd(140)
+        t2.right(90)
+        t2.fd(70)
+        t2.right(90)
+    t2.fd(110)
+    t2.left(90)
+    t2.circle(40,180)
+    t2.left(180)
+    t2.penup()
+    t2.fd(359)
+    t2.right(180)
+    t2.pendown()
+    t2.circle(40, 180)
+
+
+
+
 
 
 
@@ -52,10 +80,10 @@ def trees():
 
 def main():
     win = turtle.Screen()
-    turtle.Screen().bgcolor("#BFEDFB")
-    mountain()
-    trail()
-    trees()
+    turtle.Screen().bgcolor("#196F3D")
+    field()
+    lines()
+    golies()
     win.exitonclick()
 
 main()
