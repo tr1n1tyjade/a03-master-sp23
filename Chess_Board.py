a918beffa0da71f9982a3754f27b2be16a74055e
ayoubim <ayoubim@berea.edu>
2023-02-05 19:13:32 -0500

    Drew a chess board and a house

---
+++ Chess_Board.py
@@ -0,0 +1,123 @@
+import turtle
+
+def drawsquare (zaki, size):
+    for i in range(4):
+        zaki.forward(size)
+        zaki.left(90)
+
+def main ():
+    wn = turtle.Screen()
+    wn.bgcolor("gray")
+    zaki = turtle.Turtle()
+    zaki.fillcolor("White")
+    zaki.hideturtle()
+    zaki.speed(0)
+    zaki.penup()
+    zaki.setpos(-220, 100)
+    zaki.pendown()
+
+    for i in range(8):
+        if (i) % 2 == 0:
+            zaki.fillcolor("white")
+        else:
+            zaki.fillcolor("black")
+        zaki.begin_fill()
+        drawsquare(zaki, 60)
+        zaki.forward(60)
+        zaki.end_fill()
+
+
+    zaki.penup()
+    zaki.setpos(-220, 40)
+    zaki.pendown()
+    for i in range(8):
+        if (i) % 2 == 0:
+            zaki.fillcolor("black")
+        else:
+            zaki.fillcolor("white")
+        zaki.begin_fill()
+        drawsquare(zaki, 60)
+        zaki.forward(60)
+        zaki.end_fill()
+
+    zaki.penup()
+    zaki.setpos(-220, -20)
+    zaki.pendown()
+    for i in range(8):
+        if (i) % 2 == 0:
+            zaki.fillcolor("white")
+        else:
+            zaki.fillcolor("black")
+        zaki.begin_fill()
+        drawsquare(zaki, 60)
+        zaki.forward(60)
+        zaki.end_fill()
+
+    zaki.penup()
+    zaki.setpos(-220, -80)
+    zaki.pendown()
+    for i in range(8):
+        if (i) % 2 == 0:
+            zaki.fillcolor("black")
+        else:
+            zaki.fillcolor("white")
+        zaki.begin_fill()
+        drawsquare(zaki, 60)
+        zaki.forward(60)
+        zaki.end_fill()
+
+    zaki.penup()
+    zaki.setpos(-220, -140)
+    zaki.pendown()
+    for i in range(8):
+        if (i) % 2 == 0:
+            zaki.fillcolor("white")
+        else:
+            zaki.fillcolor("black")
+        zaki.begin_fill()
+        drawsquare(zaki, 60)
+        zaki.forward(60)
+        zaki.end_fill()
+
+    zaki.penup()
+    zaki.setpos(-220, -200)
+    zaki.pendown()
+    for i in range(8):
+        if (i) % 2 == 0:
+            zaki.fillcolor("black")
+        else:
+            zaki.fillcolor("white")
+        zaki.begin_fill()
+        drawsquare(zaki, 60)
+        zaki.forward(60)
+        zaki.end_fill()
+
+    zaki.penup()
+    zaki.setpos(-220, -260)
+    zaki.pendown()
+    for i in range(8):
+        if (i) % 2 == 0:
+            zaki.fillcolor("white")
+        else:
+            zaki.fillcolor("black")
+        zaki.begin_fill()
+        drawsquare(zaki, 60)
+        zaki.forward(60)
+        zaki.end_fill()
+
+    zaki.penup()
+    zaki.setpos(-220, -320)
+    zaki.pendown()
+    for i in range(8):
+        if (i) % 2 == 0:
+            zaki.fillcolor("black")
+        else:
+            zaki.fillcolor("white")
+        zaki.begin_fill()
+        drawsquare(zaki, 60)
+        zaki.forward(60)
+        zaki.end_fill()
+    wn.exitonclick()
+
+main()
+
---
+++ READ_ME.md
@@ -0,0 +1,3 @@
+Name: Zaki Ayoubi
+Repo link: https://github.com/Berea-College-CSC-226/a03-master-sp23.git
+Google Doc link: https://docs.google.com/document/d/1jfH5x7QutJ6QJgSUxd8SjumJCcupkgFu8Ujy9zlvfpw/edit#
