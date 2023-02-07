e0b26300a71603e85c5a73da572506b78bddf9fd
brothersd <brothersd@berea.edu>
2023-02-06 20:02:44 -0500

    code

--- Work.py
+++ Work.py
@@ -1,7 +1,7 @@
 ######################################################################
 # Author: David Brothers             TODO: Change this to your name, if modifying
 # Username: brothersd                      TODO: Change this to your username, if modifying#
-# Assignment: A02: Loopy Turtles
+# Assignment: A0  oustrophedon Turtles
 # Purpose: To demonstrate the turtle library and loops
 ######################################################################
 # Acknowledgements:
@@ -24,19 +24,20 @@ wn.bgcolor(1, 59, 200)
 turtle.pencolor()
 
 
-def bun():
+def bun(x): # Makes first bun
     turtle.begin_fill()
+    turtle.forward(30)
     for i in range(2):
         turtle.forward(80)
         turtle.right(90)
         turtle.forward(30)
         turtle.right(90)
-    turtle.fillcolor(100,90,100)
+    turtle.fillcolor(255,160,122)
     turtle.end_fill()
 
-def meat():
+def meat(): # makes meat
     turtle.begin_fill()
-    turtle.fillcolor(200,100,100)
+    turtle.fillcolor(205,92,92)
     turtle.left(120)
     turtle.forward(20)
     turtle.right(70)
@@ -49,23 +50,62 @@ def meat():
     turtle.forward(20)
     turtle.end_fill()
     turtle.right(70)
+    turtle.penup()
     turtle.forward(85)
     turtle.right(150)
-def onions():
+    turtle.down()
+def onions(): # makes Cheese
     turtle.begin_fill()
+    turtle.fillcolor(255,165,0)
     for i in range(2):
         turtle.forward(80)
         turtle.left(90)
         turtle.forward(15)
         turtle.left(90)
 
+    turtle.left(90)
+    turtle.forward(15)
+    turtle.right(90)
     turtle.fillcolor(255, 90, 100)
     turtle.end_fill()
-def main():
-    bun()
-    meat()
-    onions()
 
+def bunn(): # Makes second bunn
+    turtle.begin_fill()
+    turtle.fillcolor(255,160,122)
+    turtle.forward(-10)
+    turtle.forward(100)
+    turtle.left(90)
+
+    for i in range(10):
+        turtle.forward(3)
+        turtle.left(90)
+        turtle.forward(3)
+        turtle.right(90)
+    turtle.left(90)
+    turtle.forward(50)
+
+    for i in range(10):
+        turtle.forward(3)
+        turtle.left(90)
+        turtle.forward(3)
+        turtle.right(90)
+    turtle.forward(-10)
+    turtle.end_fill()
+    turtle.penup()
+    turtle.left(90)
+    turtle.forward(90)
+
+def Burger(x):      #Makes Borger
+        bun(x)
+        meat()
+        onions()
+        bunn()
+
+def main():
+    Burger(0)
+    Burger(30)
+    Burger(-40)
+    Burger(180)
 main()
 wn.exitonclick()
 
